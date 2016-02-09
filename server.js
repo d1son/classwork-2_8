d1d2829230b3582aca6cb2d33ae67340522e33a0
d1son <dson120@gmail.com>
2016-02-08 19:11:37 -0500

    created functions for addition, subtraction, multiplication, division functions

--- server.js
+++ server.js
@@ -8,16 +8,36 @@ app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/addition.html");
 });
 
-app.get("/", function(req, res) {
-  res.sendFile(process.cwd() + "/subtraction.html");
+app.get("/addition/:num1/:num2", function(req, res){
+  var num1 = req.params.num1;
+  var num2 = req.params.num2;
+  var sum = parseInt(num1) + parseInt(num2);
+  var sumString = sum.toString();
+  res.send(sumString);
 });
 
-app.get("/", function(req, res) {
-  res.sendFile(process.cwd() + "/multiplication.html");
+app.get("/substraction/:num1/:num2", function(req, res){
+  var num1 = req.params.num1;
+  var num2 = req.params.num2;
+  var sum = parseInt(num1) - parseInt(num2);
+  var sumString = sum.toString();
+  res.send(sumString);
 });
 
-app.get("/", function(req, res) {
-  res.sendFile(process.cwd() + "/division.html");
+app.get("/multiply/:num1/:num2", function(req, res){
+  var num1 = req.params.num1;
+  var num2 = req.params.num2;
+  var sum = parseInt(num1) * parseInt(num2);
+  var sumString = sum.toString();
+  res.send(sumString);
+});
+
+app.get("/divide/:num1/:num2", function(req, res){
+  var num1 = req.params.num1;
+  var num2 = req.params.num2;
+  var sum = parseInt(num1) / parseInt(num2);
+  var sumString = sum.toString();
+  res.send(sumString);
 });
 
 app.listen(PORT, function() {
